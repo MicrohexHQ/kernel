19dd21e4918ef932183bbc8d3c00ab688d326f4d
Atanas Bozhkov <bozhkov_atanas@gmail.com>
2012-08-11 19:53:44 +0100

    Added comments to src/loader.s

--- src/loader.s
+++ src/loader.s
@@ -1,36 +1,43 @@
-global loader
-extern kmain
+;This file initializes the stack the kernel will use, and calls “kmain,” which is a function defined in a C file.
 
-MODULEALIGN equ 1 << 0
-MEMINFO     equ 1 << 1
-FLAGS       equ MODULEALIGN | MEMINFO
-MAGIC       equ 0x1badb002
-CHECKSUM    equ -(MAGIC + FLAGS)
+global loader                           ;Making the entry point visible to the linker.
+extern kmain                            ;Kernal main is defined in kmain.cpp
+
+; Setting up the Multiboot header - see GRUB docs for details.
+
+MODULEALIGN equ 1 << 0                  ;Align loaded modules on page boundaries.
+MEMINFO     equ 1 << 1                  ;Provide memory map.
+FLAGS       equ MODULEALIGN | MEMINFO   ;This is the multiboot flag field.
+MAGIC       equ 0x1badb002              ;'Magic number' lets the bootloader find the header.
+CHECKSUM    equ -(MAGIC + FLAGS)        ;Checksum required.
 
 section .text
 align 4
 MultiBootHeader:
-    dd MAGIC
+    dd Magic                            ;Double define word.
     dd FLAGS
     dd CHECKSUM
+
+;Reserve initial kernel stack space.
 
 STACKSIZE equ 0x4000
 
 loader:
-    mov esp, stack + STACKSIZE
-    push eax
-    push ebx
+    mov esp, stack + STACKSIZE          ;Set up the stack
+    push eax                            ;Multiboot magic number
+    push ebx                            ;Multiboot info structure
 
-    call kmain
+    call kmain                          ; Call kernel.
 
     cli
 
+;Hang the machine if the kernel returns
 hang:
     hlt
     jmp hang
 
 section .bss
 align 4
-stack:
+stack:                                  ;Reserve 16k stack on a doubleword boundary.
     resb STACKSIZE
 
